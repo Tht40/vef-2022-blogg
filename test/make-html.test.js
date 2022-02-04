6a9206bcbf15a4bdbb34cb9d85627e8453246028
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-02-04 16:10:45 +0000

    first

---
+++ test/make-html.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, it } from '@jest/globals';
+import { makeHTML } from '../src/make-html';
+
+describe('html', () => {
+  it('Creates a html string', () => {
+    const input ={
+        content: '<strong>hi</strong>',
+        metadata: {
+            date: 'DATE',
+        },
+    };
+
+    const parsed = makeHTML(input);
+
+    const output = `
+    <section>
+     <strong>hi</strong>
+    <p>Skrifað: DATE</p>
+    </section>`
+        
+    expect(parsed).toBe(output);
+   });
+
+});
